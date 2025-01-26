9310530e0a0310920e816538abb41c4a7592ad73
!/usr/bin/env gh auth status -a 2/dev/null | awk 'NR==2 {print $7} <rizal.fadlullah@gmail.com>
2025-01-26 23:58:24 +0700

    fix: vscode extensions chaos control flow

--- vscode/pre_deploy.sh
+++ vscode/pre_deploy.sh
@@ -1,8 +1,7 @@
 {{#if dotter.packages.vscode}}
-if ! code --version >>/dev/null; then
+if ! command -v code >/dev/null; then
     echo "$error Failed to migrate Visual Studio Code extensions."
-    echo "$warn Visual Studio Code is either not installed or not available in \
-    the PATH."
+    echo "Visual Studio Code is either not installed or not available in the PATH."
     exit 1
 fi
 
@@ -13,16 +12,19 @@ required_extensions=(
     {{/each}}
 )
 
-for extension in ${required_extensions[@]}; do
-    if ! echo $installed_extensions | grep -q $extension; then
+# Install missing extensions
+for extension in "${required_extensions[@]}"; do
+    if ! echo "$installed_extensions" | grep -qw "$extension"; then
         echo "$info $add Installing vscode $extension extension"
-        code --install-extension $extension
+        code --install-extension "$extension"
     fi
 done
-for extension in ${installed_extensions[@]}; do
-    if ! echo $required_extensions | grep -q $extension; then
+
+# Uninstall extra extensions
+for extension in "${installed_extensions[@]}"; do
+    if ! echo "${required_extensions[@]}" | grep -qw "$extension"; then
         echo "$warn $remove Uninstalling vscode $extension extension"
-        code --uninstall-extension $extension
+        code --uninstall-extension "$extension"
     fi
 done
 {{/if}}
